6b7da4d1bf6725edcf6452c4786c7e8cbccac2b5
salma <salmalaouy83@gmail.com>
2025-12-11 17:43:39 +0100

    first commit

---
+++ crebas.sql
@@ -0,0 +1,51 @@
+/*==============================================================*/
+/* Nom de SGBD :  MySQL 5.0                                     */
+/* Date de création :  12/11/2025 5:35:59 PM                    */
+/*==============================================================*/
+
+
+drop table if exists FOUNISSAUR;
+
+drop table if exists FOURNIR;
+
+drop table if exists PRODUIT;
+
+/*==============================================================*/
+/* Table : FOUNISSAUR                                           */
+/*==============================================================*/
+create table FOUNISSAUR
+(
+   NUMF                 int not null,
+   NOMF                 text,
+   ADRESSEF             text,
+   primary key (NUMF)
+);
+
+/*==============================================================*/
+/* Table : FOURNIR                                              */
+/*==============================================================*/
+create table FOURNIR
+(
+   CODEPROD             text not null,
+   NUMF                 int not null,
+   PRIXACHAT            decimal,
+   primary key (CODEPROD, NUMF)
+);
+
+/*==============================================================*/
+/* Table : PRODUIT                                              */
+/*==============================================================*/
+create table PRODUIT
+(
+   CODEPROD             text not null,
+   DESIGPROD            text,
+   PRIXUNIT             decimal,
+   primary key (CODEPROD)
+);
+
+alter table FOURNIR add constraint FK_FOURNIR foreign key (NUMF)
+      references FOUNISSAUR (NUMF) on delete restrict on update restrict;
+
+alter table FOURNIR add constraint FK_FOURNIR2 foreign key (CODEPROD)
+      references PRODUIT (CODEPROD) on delete restrict on update restrict;
+
